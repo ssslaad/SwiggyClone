13005eb85c3c05c500ace0cecab1ebe015d72d55
Sudeep Laad <ssslaad@gmail.com>
2024-12-12 15:53:42 +0000

    refactoring and ui changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './header.css';
 import logo from '../../public/app-logo.jpg';
+import userAccountImage from '../../public/account_image.png';
 
 export default function Header() {
     return (
         <div>
             <nav className="navbar">
-                <img src={logo} alt="hi"/>
+                <img src={logo} alt="company_logo"/>
                 <ul className="nav-items">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About Us</a></li>
@@ -15,6 +16,7 @@ export default function Header() {
                         <input type="text" placeholder="Search..."/>
                         <button type="submit">Search</button>
                     </li>
+                    <li><img src={userAccountImage} alt="hi" className="userAccount"/></li>
                 </ul>
             </nav>
         </div>
