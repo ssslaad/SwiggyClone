3de20124e6c9cf6397df50efc34f039865300f8e
Sudeep <ssslaad@gmail.com>
2025-02-28 22:04:02 +0530

    Naming Convention Changes

---
+++ src/components/RestaurantMenuCard/RestaurantMenuCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function RestaurantMenuCard() {
+  const { restaurantId } = useParams();
+  return <div>RestaurantMenuCard</div>;
+}
+
+export default RestaurantMenuCard;
