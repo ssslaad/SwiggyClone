4b703fa8374f743dd09b7a668174d431040e4654
Sudeep <ssslaad@gmail.com>
2025-02-28 22:10:56 +0530

    CORS proxy URL Change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
-export const IMAGE_URL_PREFIX = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const IMAGE_URL_PREFIX =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const SWIGGY_GET_DATA_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5938761&lng=73.74799589999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_GET_DATA_API =
+  "https://proxy.cors.sh/" +
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5938761&lng=73.74799589999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const CLIENT_ID = "107248026856-7c7g59vs83h7gdri1cqt9da0pus58k3c.apps.googleusercontent.com";
+export const CLIENT_ID =
+  "107248026856-7c7g59vs83h7gdri1cqt9da0pus58k3c.apps.googleusercontent.com";
